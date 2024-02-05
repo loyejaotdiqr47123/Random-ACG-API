caf4318b5abf971474c63af5cd74444b4d340bd5
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-02-05 01:37:11 +0000

    up

--- pe.php
+++ pe.php
@@ -1,14 +1,20 @@
 <?php
 //读取文本
 $str = explode("\n", file_get_contents('pe.txt'));
+//随机选择一个
 $k = rand(0,count($str));
+//解析图片
 $sina_img = str_re($str[$k]);
 
+//定义尺寸数组
 $size_arr = array('kf', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
+//获取尺寸
 $size = !empty($_GET['size']) ? $_GET['size'] : 'kf' ;
+//判断尺寸是否在尺寸数组中
 if(!in_array($size, $size_arr)){
 	$size = 'large';
 }
+//解析图片地址
 $url = ''.$sina_img.'';
 //解析结果
 $result=array("code"=>"200","acgurl"=>"$url");
@@ -19,17 +25,23 @@ switch ($type)
    
 //格式解析                           
 case 'json':
+//解析图片地址
 $path = "$url";
+//解析图片信息
 $pathinfo = pathinfo($path);
 $imageInfo = getimagesize($url);  
+//解析图片宽高
 $result['width']="$imageInfo[0]";  
 $result['height']="$imageInfo[1]";
+//解析图片格式
 $result['size']="$pathinfo[extension]";    
+//输出json格式
 header('Content-type:text/json');
 echo json_encode($result);
 break;
 //格式解析                             
 case 'img':
+//解析图片地址
 $img = file_get_contents($url,true);
 //使用图片头输出浏览器
 header("Content-Type: image/jpeg;");
@@ -37,9 +49,11 @@ echo $img;
 break;
 //IMG
 default:
+//输出图片地址
 header("Location:".$result['acgurl']);
 break;
 }
+//替换空格
 function str_re($str){
   $str = str_replace(' ', "", $str);
   $str = str_replace("\n", "", $str);
