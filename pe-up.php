18591515f520d1854a7d1717364efabfc4fe4b23
bobo <bobo134662@proton.me>
2024-06-16 05:14:04 +0000

    重构

--- pe-up.php
+++ pe-up.php
@@ -1,12 +1,5 @@
 <?php
-// 检查pe.txt是否存在，不存在则建新
-//if (!file_exists('pe.txt')) {
-//$file = fopen('pe.txt', 'w');
-//fclose($file);
-//}
-//定义访问ua为手机
 $ua = 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.0; )';
-//访问https://acg.suyanw.cn/api.php，并且获取访问后302的地址为a
 $url = 'https://acg.suyanw.cn/api.php';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -18,32 +11,28 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $output = curl_exec($ch);
 $a = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
 curl_close($ch);
-// 定义异常值1
+
 $a1 = 'https://acg.suyanw.cn/api.php';
-// 检查a是否等于异常值1
 if ($a == $a1) {
-    // 输出异常值1
     echo '异常值1';
     exit;
 }
-// 定义异常值2
+
 $a2 = 'https://i3.wp.com/wx4.sinaimg.cn/large/.jpg';
-// 检查a是否等于异常值2
 if ($a == $a2) {
-    // 输出异常值2
     echo '异常值2';
     exit;
 }
-// 检查pe.txt是否包含a
+
 if (file_exists('pe.txt')) {
     $pe = file_get_contents('pe.txt');
     if (strpos($pe, $a) !== false) {
         echo '已存在';
         exit;
     } else {
-        // 将回车写入pe.txt，然后再写入a
         file_put_contents('pe.txt', PHP_EOL . $a, FILE_APPEND);
         echo '写入成功';
         exit;
     }
 }
+?>
--- pe.php
+++ pe.php
@@ -1,63 +1,42 @@
 <?php
-//读取文本
-$str = explode("\n", file_get_contents('pe.txt'));
-//随机选择一个
-$k = rand(0,count($str));
-//解析图片
-$sina_img = str_re($str[$k]);
 
-//定义尺寸数组
-$size_arr = array('kf', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
-//获取尺寸
-$size = !empty($_GET['size']) ? $_GET['size'] : 'kf' ;
-//判断尺寸是否在尺寸数组中
-if(!in_array($size, $size_arr)){
-	$size = 'large';
+$fileContent = file_get_contents('pe.txt');
+$lines = explode("\n", $fileContent);
+$randomLine = $lines[array_rand($lines)];
+$sinaImg = str_replace([' ', "\n", "\t", "\r"], '', $randomLine);
+
+$sizeArr = ['kf', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square'];
+$size = isset($_GET['size']) ? $_GET['size'] : 'kf';
+if (!in_array($size, $sizeArr)) {
+    $size = 'large';
 }
-//解析图片地址
-$url = ''.$sina_img.'';
-//解析结果
-$result=array("code"=>"200","acgurl"=>"$url");
-//Type Choose参数代码
-$type=$_GET['return'];
-switch ($type)
-{   
-   
-//格式解析                           
-case 'json':
-//解析图片地址
-$path = "$url";
-//解析图片信息
-$pathinfo = pathinfo($path);
-$imageInfo = getimagesize($url);  
-//解析图片宽高
-$result['width']="$imageInfo[0]";  
-$result['height']="$imageInfo[1]";
-//解析图片格式
-$result['size']="$pathinfo[extension]";    
-//输出json格式
-header('Content-type:text/json');
-echo json_encode($result);
-break;
-//格式解析                             
-case 'img':
-//解析图片地址
-$img = file_get_contents($url,true);
-//使用图片头输出浏览器
-header("Content-Type: image/jpeg;");
-echo $img;
-break;
-//IMG
-default:
-//输出图片地址
-header("Location:".$result['acgurl']);
-break;
+$url = $sinaImg;
+$result = ["code" => "200", "acgurl" => $url];
+$type = $_GET['return'];
+
+switch ($type) {
+    case 'json':
+        $path = $url;
+        $pathinfo = pathinfo($path);
+        $imageInfo = getimagesize($url);
+        $result['width'] = $imageInfo[0];
+        $result['height'] = $imageInfo[1];
+        $result['size'] = $pathinfo['extension'];
+        header('Content-type:text/json');
+        echo json_encode($result);
+        break;
+    case 'img':
+        $img = file_get_contents($url, true);
+        header("Content-Type: image/jpeg;");
+        echo $img;
+        break;
+    default:
+        header("Location:".$result['acgurl']);
+        break;
 }
-//替换空格
-function str_re($str){
-  $str = str_replace(' ', "", $str);
-  $str = str_replace("\n", "", $str);
-  $str = str_replace("\t", "", $str);
-  $str = str_replace("\r", "", $str);
-  return $str;
+
+function str_replace_array($search, $replace, $subject) {
+    return str_replace($search, $replace, $subject);
 }
+
+?>
