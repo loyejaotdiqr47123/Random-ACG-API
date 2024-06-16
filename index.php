f1edf3e8537e1fb7862fd41f3745c4c0ece97079
bobo <bobo134662@proton.me>
2024-06-16 04:34:59 +0000

    修复了随机二次元图片API的文件读取错误，并优化了页面结构。

--- index.php
+++ index.php
@@ -1,59 +1,12 @@
 <?php
-// 统计pc.txt和pe.txt行数为a
-$pc = file('pc.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$pe = file('pe.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$a = count($pc) + count($pe);
-//计算pc.txt文件更改时间的时间缀
-$pc_time = filemtime('pc.txt');
-//计算pe.txt文件更改时间的时间缀
-$pe_time = filemtime('pe.txt');
-//比较时间缀，取较大的为b
-$b = max($pc_time, $pe_time);
-// 将时间缀b转换为正常时间，年月日之间用-相连
-$b = date('Y-m-d', $b);
+$pc = file('pc.txt', FILE_IGNORE_NEW_LINES);
+$pe = file('pe.txt', FILE_IGNORE_NEW_LINES);
+$total_images = count($pc) + count($pe);
+
+$last_updated = date('Y-m-d', max(filemtime('pc.txt'), filemtime('pe.txt')));
+
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'http';
+$host = $protocol . '://' . $_SERVER['HTTP_HOST'];
+
+require_once 'template.php';
 ?>
-<html>
-
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<title>随机二次元图片API - ACGAPI接口站</title>
-	<meta name="keywords" content="接口口站,图库,二次元图片,二次元API,动漫图片API,图片API">
-	<meta name="description" content="随机二次元图片API,随机动漫壁纸,每天刷一刷，每次不一样。">
-	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
-
-<body>
-	<div>
-		<p algin=“center” class="title title2">随机二次元图片API</p>
-	</div>
-	<div class="container">
-		<div class="well">
-			<h3>随机二次元图片API</h3>
-			<p>目前系统共收录
-				<?php echo $a; ?> 张图片 API最后更新时间:
-				<?php echo $b; ?>
-			</p>
-			<p>图片均为https，高速访问</p>
-			<p>自动爬取第三方api图片</p>
-			<p>自判断二次元API基本调用格式：</p>
-			<pre><a href="<?php $_SERVER['HTTP_HOST'] ?>/api.php" target="_blank"><?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/api.php</a></pre>
-			<p>PC随机二次元API</p>
-			<pre><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/pc.php" target="_blank"><?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/pc.php</a></pre>
-			<p>PE随机二次元API</p>
-			<pre><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/pe.php" target="_blank"><?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/pe.php</a></pre>
-			<p>请求方式</p>
-			<pre>get/post</pre>
-			<p>返回格式</p>
-			<pre>json/images</pre>
-		</div>
-	</div>
-	<div class="bottom">
-
-		<p>随机二次元API开源----github<a href="https://github.com/loyejaotdiqr47123/Random-ACG-API" target="_blank"
-				class="one-pan-link-mark"></a></p>
-	</div>
-</body>
-
-</html>
---
+++ template.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>随机二次元图片API</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <div class="container">
+        <h2>随机二次元图片API</h2>
+        <div class="well">
+            <h3>API文档</h3>
+            <p>共有 <?php echo $total_images; ?> 张图片<br>图片最后更新:<?php echo $last_updated; ?></p>
+            <p>调用二次元API的基本格式是：</p>
+            <pre><code><?php echo $host; ?>/api.php</code></pre>
+            <p>PC随机二次元API</p>
+            <pre><code><?php echo $host; ?>/pc.php</code></pre>
+            <p>PE随机二次元API</p>
+            <pre><code><?php echo $host; ?>/pe.php</code></pre>
+            <p>请求：GET/POST<br>响应：JSON/images</p>
+        </div>
+    </div>
+    <div class="bottom">
+        <p><a href="https://github.com/loyejaotdiqr47123/Random-ACG-API">开源仓库</a></p>
+        <p>ICP备案号：<a href="https://beian.miit.gov.cn" target="_blank">吉ICP备2024010762号-2</a></p>
+    </div>
+</body>
+</html>
